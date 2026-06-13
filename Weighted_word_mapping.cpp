1dab9f544a40e55ceb504e33c078df685f23bbb4
Kathanshah5510 <kathanshah5511@gmail.com>
2026-06-13 10:26:44 +0530

    Refactored input handling for weighted word mapping to allow dynamic user input for problem 3838

--- Weighted_word_mapping.cpp
+++ Weighted_word_mapping.cpp
@@ -2,17 +2,22 @@
 using namespace std;
 
 int main(){
-    // int n;
-    // cout << "Enter the number of strings: ";
-    // cin >> n;
-    // vector<string> words(n);
-    // cout << "Enter the strings:\n";
-    // for(int i = 0; i < n; i++){
-        // cin >> words[i];
-    // }
-    int n = 3;
-    vector<string> words = {"abcd", "def", "xyz"};
-    vector<int> weights = {5,3,12,14,1,2,3,2,10,6,6,9,7,8,7,10,8,9,6,9,9,8,3,7,7,2};
+    int n;
+    cout << "Enter the number of strings: ";
+    cin >> n;
+    vector<string> words(n);
+    cout << "Enter the strings:\n";
+    for(int i = 0; i < n; i++){
+        cin >> words[i];
+    }
+    vector<int> weights(26);
+    cout << "Enter the weights for each letter (a-z):\n";
+    for(int i = 0; i < 26; i++){
+        cin >> weights[i];
+    }
+    // int n = 3;
+    // vector<string> words = {"abcd", "def", "xyz"};
+    // vector<int> weights = {5,3,12,14,1,2,3,2,10,6,6,9,7,8,7,10,8,9,6,9,9,8,3,7,7,2};
     
     string result = "";
     for(string word : words){
